ad633b50726836c4fe57a9f73fd0f1399586becd
Deon Saunders <51129091+degrasse-python@users.noreply.github.com>
2024-10-04 14:20:44 -0400

    update api

--- main.py
+++ main.py
@@ -21,12 +21,13 @@ BACKEND_DB = os.environ['BACKEND_DB']
 BACKEND_USER = os.environ['BACKEND_USER']
 BACKEND_PASS = os.environ['BACKEND_PASS']
 
+
+import redis
+
 # connect to redis
 rd = redis.Redis(host=BACKEND_URI, 
                  port=BACKEND_PORT, 
-                 db=BACKEND_DB,
-                 username="default", # use your Redis user. More info https://redis.io/docs/latest/operate/oss_and_stack/management/security/acl/
-                 password="secret",) # use your Redis password
+                 password=BACKEND_PASS,) # use your Redis password
                  #ssl=True,
                  #ssl_certfile="./redis_user.crt",
                  #ssl_keyfile="./redis_user_private.key",
@@ -88,35 +89,3 @@ async def postDailyDewPoint2mMean(weather: Weather):
     rd.set(str(weather), encodedNumpyData)
     return f"Current {encodedNumpyData}"
   
-
-@app.post("/test_model/")
-async def testModel(weather: Weather):
-  # check cache
-  cache = rd.get(str(weather))
-  if cache:
-    print("cache hit")
-    print(json.loads(cache))
-    return json.loads(cache)
-  else:
-    print("cache miss")
-    weather_dict = weather.model_dump()
-    # request get from open weather api
-    params = {
-          "latitude": weather.latitude, #52.5
-          "longitude": weather.longitude, #13.41
-          "start_date": weather.start, #"2024-01-01"
-          "end_date": weather.end, # "2024-01-02"
-          "models": ["CMCC_CM2_VHR4", "FGOALS_f3_H", "HiRAM_SIT_HR", "MRI_AGCM3_2_S", "EC_Earth3P_HR", "MPI_ESM1_2_XR", "NICAM16_8S"],
-          "daily": "dew_point_2m_mean"
-        }
-    responses = CLIENT.weather_api(URL, params=params)
-    response = responses[0]
-    daily = response.Daily()
-    daily_dew_point_2m_mean = daily.Variables(0).ValuesAsNumpy()
-    print(f"Daily Dew Point 2mim Mean {daily_dew_point_2m_mean}")
-    print(f"Weather: {str(weather)}")
-    encodedNumpyData = json.dumps(daily_dew_point_2m_mean, cls=NumpyArrayEncoder)
-    # TODO: return actual json data that looks decent
-    rd.set(str(weather), encodedNumpyData)
-    return f"Current {encodedNumpyData}"
-
